e4eead04ece9fb98d1d5defcb1bd8949756c5a69
Guru <gurudayaluday2003@gmail.com>
2024-10-20 18:21:52 +0000

    Third commit

--- app/test.php
+++ app/test.php
@@ -1,6 +1,16 @@
 
 <pre><?
+print("SERVER\n");
 print_r($_SERVER);
+print("GET\n");
+print_r($_GET);
+print("POST\n");
+print_r($_POST);
+print_r("COOKIE\n");
+print_r($_COOKIE);
+print_r("FILE\n");
+print_r($_FILES);
+
 
 ?>
 </pre>

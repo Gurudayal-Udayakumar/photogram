16e31f09dcf7161dde2fad9cf31eb175b3295c61
Guru <gurudayaluday2003@gmail.com>
2024-11-24 18:18:49 +0000

    session introduction

--- app/sg.php
+++ app/sg.php
@@ -1,17 +1,28 @@
-<pre><?php
+<pre>
+<?php
 session_start();
-setcookie("testcookie","testvalue",time()+(86400*30),"/");
+//setcookie("testcookie","testvalue",time()+(86400*30),"/");
 include 'libs/load.php';
 
-print_r("SESSION\n");
+print_r("_SESSION\n");
 print_r($_SESSION);
 
+if(isset($_GET['clear'])){
+    print("Clearing session......\n");
+    session_unset();
+}
 
-if($a){
-    print("A already exists......Value: $a\n");
+if(isset($_GET['destroy'])){
+    print("Destroying....\n");
+    session_destroy();
+}
+
+
+if(isset($_SESSION['a'])){
+    print("A already exists......Value: $_SESSION[a]\n");
 }else{
-    $a = time();
-    print("Assigning new value.... Value: $a\n");
+    $_SESSION['a'] = time();
+    print("Assigning new value.... Value:b$_SESSION[a]\n");
 }
 
 ?>

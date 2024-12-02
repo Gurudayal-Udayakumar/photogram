25aa080e54aaad37d4def251f574728a87c59232
Guru <gurudayaluday2003@gmail.com>
2024-12-02 16:02:45 +0000

    php sessions

--- app/sg.php
+++ app/sg.php
@@ -7,23 +7,28 @@ include 'libs/load.php';
 print_r("_SESSION\n");
 print_r($_SESSION);
 
+
 if(isset($_GET['clear'])){
-    print("Clearing session......\n");
+    printf("Clearing session......\n");
     session_unset();
 }
 
-if(isset($_GET['destroy'])){
-    print("Destroying....\n");
-    session_destroy();
-}
-
-
 if(isset($_SESSION['a'])){
-    print("A already exists......Value: $_SESSION[a]\n");
+    printf("A already exists......Value: $_SESSION[a]\n");
 }else{
     $_SESSION['a'] = time();
-    print("Assigning new value.... Value:b$_SESSION[a]\n");
+    printf("Assigning new value.... Value:b$_SESSION[a]\n");
 }
 
+
+// if(isset($_GET['destroy'])){
+//     printf("destroying....\n");
+//     session_destroy();
+
+// }
+
+
+print_r("_SERVER");
+print_r($_SERVER);
 ?>
 </pre>

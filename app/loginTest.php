030913b361dbb987084edc0955eba548547bead0
Guru <gurudayaluday2003@gmail.com>
2024-12-12 13:17:05 +0000

    setup of simple login and logout

--- app/loginTest.php
+++ app/loginTest.php
@@ -5,11 +5,29 @@ Session::start();
 
 $user = "hari";
 $pass = "hari";
+$result = null;
 
-$result = User::login($user,$pass);
-if($result){
-    echo"Login successful ,$result[username]";
+if(isset($_GET['logout'])){
+    Session::destroy();
+    die("Session destroyed , <br><a href='loginTest.php'>Login Again</a>");
 }
-else{
-    echo"Login failed";
+
+if(Session::get('is_loggedin')){
+    $userdata = Session::get('session_user');
+    print("Welcome back , $userdata[username]");
+    $result = $userdata;
+}else{
+    printf("No session found , try login now.");
+    $result = User::login($use,$pass);
+    if($result){
+        echo"Login successful ,$result[username]";
+        Session::set('is_loggedin',true);
+        Session::set('session_user',$result);
+    }
+    else{
+        echo"Login failed";
+    }
 }
+echo <<<EOL
+<br><a href = "loginTest.php?logout">Logout</a>
+EOL;
